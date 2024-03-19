53d4879cb68f190ad079666c80f278abcd421095
Sarfaraz Shaikh <154805542+shahmeershaikh@users.noreply.github.com>
2024-03-19 19:08:19 +0500

    SMIT Project

---
+++ app.js
@@ -0,0 +1,42 @@
+// 1. Declare a variable called username.
+let username;
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+let myName = "Alice";
+
+// 3. Write script to
+//    a) Declare a JS variable, titled message.
+//    b) Assign “Hello World” to variable message
+//    c) Display the message in alert box.
+let message;
+message = "Hello Words";
+alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+let studentName = "Sarfaraz shaikh";
+let age = 18;
+let gender = "Male";
+let country = "Pakistan";
+
+alert("Student's Name: " + studentName);
+alert("Age: " + age);
+alert("Gender: " + gender);
+alert("Country: " + country);
+// 5. Write a script to display the following alert using one JS variable:
+let alertMessage = "I'm learning JavaScript!";
+alert(alertMessage);
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+let email = "tosarafarz32@gmail.com";
+alert("My email address is: " + email);
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+let book = "A smarter way to learn JavaScript";
+alert("I am reading the book titled: '" + book + "'");
+// 8. Write a script to display this in browser through JS
+document.write("Hello, this is displayed in the browser through JavaScript.");
+
+// 9. Store following string in a variable and show in alert and browser through JS
+let specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(specialString);
+document.write("<br>" + specialString);
